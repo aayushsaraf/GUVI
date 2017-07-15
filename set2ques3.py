ab327ecc33b1a5791a7c5afcb2d2d6b947e83f04
aayushsaraf <aayushsaraf14@gmail.com>
2017-07-15 14:26:29 +0530

    Add files via upload

---
+++ set2ques3.py
@@ -0,0 +1,12 @@
+print("ENTER THE NUMBER")
+num=int(input())
+count = 0
+for i in range(1,num+1):
+	if(num%i==0):
+		count=count+1
+if(count==2):
+	print("PRIME NUMBER")
+else:
+	print("NOT A PRIME NUMBER")
+
+
